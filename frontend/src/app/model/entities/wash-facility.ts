26bd2f4fedd3d6c0d21e7cde71247a939f8f06aa
paulscv <paulscv@gmail.com>
2017-06-20 01:29:10 +0200

    добавлены Телефоны к Мойками

--- frontend/src/app/model/entities/wash-facility.ts
+++ frontend/src/app/model/entities/wash-facility.ts
@@ -2,6 +2,7 @@ import {Address} from "./address";
 import {User} from "./user";
 import {WashBox} from "./wash-box";
 import {BaseMoikaEntity} from "./base-moika-entity";
+import {Phone} from "./phone";
 
 export class WashFacility extends BaseMoikaEntity {
    id: number;
@@ -9,6 +10,7 @@ export class WashFacility extends BaseMoikaEntity {
    name: string;
    description: string;
    idAddr:  number;
+   phones: Array<Phone>;
    facilityAddr: Address;
    idManager: number;
    manager: User;
